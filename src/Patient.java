3f2e5ad863f6760a264279d53ce9c5918b7f95a4
Абибулла Зарина Әділханқызы <253333@astanait.edu.kz>
2025-12-11 13:15:45 +0500

    3

--- src/Patient.java
+++ src/Patient.java
@@ -1,3 +1,27 @@
 public class Patient {
     private String name;
+    private String surname;
+    private int IIN;
+    private String phonenumber;
+    public Patient(String name, String surname,int IIN, String phonenumber){
+        this.name=name;
+        this.surname=surname;
+        this.IIN=IIN;
+        this.phonenumber=phonenumber;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getSurname(){
+        return surname;
+    }
+    public int getIIN(){
+        return IIN;
+    }
+    public String getPhonenumber(){
+        return phonenumber;
+    }
+
+
+
 }
