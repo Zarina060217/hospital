6e393de5d438696119b70226bdb9d72dd9d13a86
Абибулла Зарина Әділханқызы <253333@astanait.edu.kz>
2025-12-22 22:29:17 +0500

    14

--- src/Patient.java
+++ src/Patient.java
@@ -21,23 +21,47 @@ public class Patient {
     public String getBloodtype(){
         return bloodtype;
     }
-    public void setIIN(IIN){
+    public void setIIN(String IIN){
         this.IIN=IIN;
     }
-    public void setFullname(fullname){
+    public void setFullname(String fullname){
         this.fullname=fullname;
     }
-    public void setAge(age){
+    public void setAge(int age){
         this.age=age;
     }
-    public void setBloodtype(bloodtype){
+    public void setBloodtype(String bloodtype){
         this.bloodtype=bloodtype;
     }
     public boolean isMinor(){
         return age<18;
     }
-    public void getagecategory(double category){
-        return category=age/10;
+    public String getcategory(){
+        if (age<18) return "infant";
+        if (age>63) return "Retired";
+        else return "Adult";
+    }
+    public enum Bloodtype{
+        O, A,B,AB
+    }
+    public String isDonorfor(Bloodtype bloodtype) {
+        switch (bloodtype) {
+            case O -> {
+                return fullname + "universal donor";
+            }
+            case A -> {
+                return fullname + "is donor for A and AB";
+            }
+            case B -> {
+                return fullname + "is donor for B and AB";
+            }
+            case AB -> {
+                return fullname + "is universal recipient";
+            }
+            default -> {
+                return "Invalid blood type";
+            }
+        }
     }
 
     public String toString(){
