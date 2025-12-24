3952748e732578cc44b58f0941c2fd825669d607
Абибулла Зарина Әділханқызы <253333@astanait.edu.kz>
2025-12-24 18:03:44 +0500

    18

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,84 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.time.LocalDateTime;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main{
+    public static void main(String[] args){
+        System.out.println("===Hospital Management System===");
+        System.out.println();
+        Patient patient1=new Patient("060217789123","Dosymova Zhansaya",17,Patient.Bloodtype.AB);
+        Patient patient2=new Patient("091207589632","Beisen Bori",13,Patient.Bloodtype.O);
+        Patient patient3=new Patient("100511789583","Nazar Hadjar",13,Patient.Bloodtype.A);
+        Doctor doctor1=new Doctor("D01","Beisen Zamira","therapist",30);
+        Doctor doctor2=new Doctor("D02","Yesirkep Yelnaz","gynecologist",1);
+        Doctor doctor3=new Doctor("D03","Shabden Adil","radiologist",13);
+        LocalDateTime date= LocalDateTime.of(2025,12,28,14,0);
+        Appointment appointment1=new Appointment("A01",patient2.getFullname(),doctor3.getName(),date,"scheduled");
+        Appointment appointment2=new Appointment("A02",patient3.getFullname(),doctor2.getName(),date,"scheduled");
+
+        System.out.println("---Patients---");
+        System.out.println(patient1);
+        System.out.println(patient2);
+        System.out.println(patient3);
+        System.out.println();
+        System.out.println("---Doctors---");
+        System.out.println(doctor1);
+        System.out.println(doctor2);
+        System.out.println(doctor3);
+        System.out.println();
+        System.out.println("---Appointments---");
+        System.out.println(appointment1);
+        System.out.println(appointment2);
+        System.out.println();
+
+        System.out.println("---Testing getters---");
+        System.out.println("Patient 1 name:"+patient1.getFullname());
+        System.out.println("Patient 3 bloodtype"+patient3.getBloodtype());
+        System.out.println("Doctor 3 specialization:"+doctor1.getSpecialization());
+        System.out.println("Appointment 2 date:"+appointment2.getDate());
+        System.out.println();
+
+        System.out.println("---Testing setters---");
+        System.out.println("Updating patient3...");
+        patient3.setFullname("Niyzabek Kausar");
+        patient3.setAge(15);
+        patient3.setBloodtype(Patient.Bloodtype.B);
+        System.out.println("Updated:"+patient3);
+        System.out.println();
+        System.out.println("Changing appointment2...");
+        appointment2.setStatus("Unscheduled");
+        System.out.println("Changed:"+appointment2);
+        System.out.println();
+
+        System.out.println("___Testing patient mathods___");
+        String compatability=patient1.getDonorCompatibility();
+        System.out.println(compatability);
+        System.out.println(patient2.getDonorCompatibility());
+        System.out.println(patient3.getFullname()+" is Minor: "+patient3.isMinor());
+        System.out.println(patient2.getFullname()+" is "+patient2.getCategory());
+        System.out.println("___Testing doctor mathods___");
+        System.out.println(doctor2.getName()+"could perform surgery:"+doctor2.canPerformSurgery());
+        System.out.println(doctor3.getName()+"is experienced: "+doctor3.isExperienced());
+        System.out.println("___Testing appointment mathods___");
+        appointment2.cancel();
+        System.out.println("Appointment 2 after cancellation:"+ appointment2);
+        LocalDateTime newDate=LocalDateTime.of(2023,12,30,9,0);
+        appointment1.reschedule(newDate);
+        System.out.println("appointment1 after reschedule"+ appointment1);
+
+        System.out.println("--- FINAL STATE ---");
+        System.out.println("---Patients---");
+        System.out.println(patient1);
+        System.out.println(patient2);
+        System.out.println(patient3);
+        System.out.println();
+        System.out.println("---Doctors---");
+        System.out.println(doctor1);
+        System.out.println(doctor2);
+        System.out.println(doctor3);
+        System.out.println();
+        System.out.println("---Appointments---");
+        System.out.println(appointment1);
+        System.out.println(appointment2);
+        System.out.println();
+        System.out.println("\n=== Program Complete ===");
     }
 }
--- src/Patient.java
+++ src/Patient.java
@@ -2,8 +2,8 @@ public class Patient {
     private String IIN;
     private String fullname;
     private int age;
-    private String bloodtype ;
-    public Patient(String IIN, String fullname,int age, String bloodtype){
+    private Bloodtype bloodtype ;
+    public Patient(String IIN, String fullname,int age, Bloodtype bloodtype){
         this.IIN=IIN;
         this.fullname=fullname;
         this.age=age;
@@ -18,7 +18,7 @@ public class Patient {
     public int getAge(){
         return age;
     }
-    public String getBloodtype(){
+    public Bloodtype getBloodtype(){
         return bloodtype;
     }
 
@@ -31,41 +31,30 @@ public class Patient {
     public void setAge(int age){
         this.age=age;
     }
-    public void setBloodtype(String bloodtype){
+    public void setBloodtype(Bloodtype bloodtype){
         this.bloodtype=bloodtype;
     }
 
     public boolean isMinor(){
         return age<18;
     }
-    public String getcategory(){
+    public String getCategory(){
         if (age<18) return "infant";
         if (age>63) return "Retired";
         else return "Adult";
     }
-    public enum Bloodtype{
-        O, A,B,AB
+    public enum Bloodtype {
+        O, A, B, AB
     }
-    public String isDonorfor(Bloodtype bloodtype) {
-        switch (bloodtype) {
-            case O -> {
-                return fullname + "universal donor";
-            }
-            case A -> {
-                return fullname + "is donor for A and AB";
-            }
-            case B -> {
-                return fullname + "is donor for B and AB";
-            }
-            case AB -> {
-                return fullname + "is universal recipient";
-            }
-            default -> {
-                return "Invalid blood type";
-            }
+    public String getDonorCompatibility() {
+        switch (this.bloodtype) {
+            case O -> { return fullname + " is a universal donor"; }
+            case A -> { return fullname + " is a donor for A and AB"; }
+            case B -> { return fullname + " is a donor for B and AB"; }
+            case AB -> { return fullname + " is a universal recipient"; }
+            default -> { return "Unknown blood type"; }
         }
     }
-
     public String toString(){
         return "Patient{IIN='"+IIN+"', fullname="+fullname+", age='"+age+"',bloodttype="+bloodtype+"}";
     }
